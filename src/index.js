1933e0bd1846465082a1a6a5c415f04d90145cfa
Nici Putter <niciputter@yahoo.com>
2019-02-04 15:12:04 +0100

    index.js, Hello.jsx files configured - incl header.jsx and footer.jsx

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Header from "./Header"
+import Hello from "./Hello"
+import Footer from "./Footer"
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <Hello />
+            <Footer />
+        </div>
+    )
+};
+
+ReactDOM.render(<App />, document.getElementById("app"))
