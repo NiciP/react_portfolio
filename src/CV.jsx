3a2e29870bb516942160bf5d9f9367164fd8e012
Nici Putter <niciputter@yahoo.com>
2019-02-09 12:34:41 +0100

    CVcard created and configured

--- src/CV.jsx
+++ src/CV.jsx
@@ -1,11 +1,56 @@
-import React from "react"
+import React, { Component } from "react"
+import axios from "axios"
+import CVcard from "./CVcard"
 
-const CV = () =>  {
-    return (
-        <div>
-            <h1 className="content-text">My CV</h1>
-        </div>
-    )
-}
+class Cv extends Component {
+    constructor() {
+        super();
+        this.state = {
+            cvitems: []
+        };
+    }
 
-export default CV
+    render() {
+        const cvitems = this.state.cvitems
+        let cvItemsList 
+
+        if (cvitems.length > 0) {
+            cvItemsList = cvitems.map(cvitem => {
+                return (
+                    <div key={cvitem.id} className="min-h-900 my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3"><CVcard cvitem={cvitem}/></div>
+                )
+            })
+        }
+
+
+        return (
+            <div>
+                <h1 className="text-white font-sans ml-20 pt-6 pb-4">My Career summary...</h1>
+                {cvItemsList}
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        axios.get('./src/data/cv.json')
+            .then(response => {
+                this.setState({
+                    cvitems: response.data
+                })
+            })
+    }
+
+
+};
+
+export default Cv
+
+
+
+//const CV = () =>  {
+  //  return (
+    //    <div>
+    //        <h1 className="content-text">My CV</h1>
+    //    </div>
+   /// )
+//}
--- src/CVcard.jsx
+++ src/CVcard.jsx
@@ -1,16 +1,16 @@
 import React from "react"
 
-const ProjectCard = (props) => {
-    let project = props.project
+const CVcard = (props) => {
+    let cvitem = props.cvitem
     return (
 
-        <div key={project.id} className="ml-16 overflow-hidden">
-            <a href={project.link} target="_blank">
+        <div key={cvitem.id} className="ml-16 overflow-hidden">
+            <a>
                 <img src={project.image} className="block h-auto w-full" style={{ height: '350px', objectFit: 'cover' }} />
                 </a>
                 <div className="px-2 py-4" style={{ minHeight: '80px' }} >
-                    <div className="text-white font-bold text-2xl mb-2">{project.name}</div>
-                    <p className="text-white text-base">{project.description}</p>    
+                    <div className="text-white font-bold text-2xl mb-2">{cvitem.position}</div>
+                    <p className="text-white text-base">{cvitem.company}</p>    
                 </div>
 
                 <div className="flex items-center justify-between leading-none md:p-4">
@@ -21,4 +21,4 @@ const ProjectCard = (props) => {
     )
 }
 
-export default ProjectCard
+export default CVcard
