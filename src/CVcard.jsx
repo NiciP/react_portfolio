cd90bdfc0e5580e09088d6ddeade226c7c9a009f
Nici Putter <niciputter@yahoo.com>
2019-02-09 22:49:43 +0100

    CV cards importing .json correctly, working on education import

--- src/CVcard.jsx
+++ src/CVcard.jsx
@@ -5,19 +5,34 @@ const CVcard = (props) => {
     return (
 
         <div key={cv.id} className="ml-16 overflow-hidden">
-                <img src={cv.Position} className="block h-auto w-full" style={{ height: '350px', objectFit: 'cover' }} />
+                
                 <div className="px-2 py-4" style={{ minHeight: '80px' }} >
                     <div className="text-white font-bold text-2xl mb-2">{cv.Position}</div>
-                    <p className="text-white text-base">
-                    {cv.Company}</p>    
+                    <p className="text-white text-lg mb-2">{cv.Company}</p>    
+                    <p className="text-xs text-white">{cv.Date}</p> 
                 </div>
 
                 <div className="flex items-center justify-between leading-none md:p-4">
-                    <button className="text-sm bg-green-darker hover:bg-blue-dark text-white text-xs py-1 px-2 rounded-full">
+                    <button className="text-sm bg-green-darker hover:bg-blue-dark text-white text-xs py-1 px-4 rounded-full">
                         Details
                     </button>
                 </div>
-           
+        </div>
+
+        <div key={cv.eid} className="ml-16 overflow-hidden">
+                                
+                <div className="px-2 py-4" style={{ minHeight: '80px' }} >
+                    <div className="text-white font-bold text-2xl mb-2">{cv.Education}</div>
+                    <p className="text-white text-lg mb-2">{cv.Institution}</p>    
+                    <p className="text-xs text-white">{cv.Dates}</p> 
+                </div>
+
+                <div className="flex items-center justify-between leading-none md:p-4">
+                    <button className="text-sm bg-green-darker hover:bg-blue-dark text-white text-xs py-1 px-4 rounded-full">
+                        Details
+                    </button>
+                </div>
+
         </div>
     )
 }
--- src/Cv.jsx
+++ src/Cv.jsx
@@ -6,17 +6,9 @@ class Cv extends Component {
     constructor() {
         super();
         this.state = {
-            cvs: []
+            cvs: [],
+            edu: []
         };
-    }
-
-        componentDidMount() {
-            axios.get('./src/data/cv.json')
-                .then(response => {
-                    this.setState({
-                        cvs: response.data
-                    })
-                })
     }
 
     render() {
@@ -26,11 +18,22 @@ class Cv extends Component {
         if (cvs.length > 0) {
             cvList = cvs.map(cv => {
                 return (
-                    <div key={cvs.id} className="min-h-900 my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-                        <CVcard cv={cv}/>
-                    </div>
+                    <div key={cvs.id} className="min-h-2000 my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3"><CVcard cv={cv}/></div>
                 )
             })
+        }
+
+        render() {
+            const edu = this.state.edu
+            let eduList 
+    
+            if (edu.length > 0) {
+                eduList = edu.map(ed => {
+                    return (
+                        <div key={edu.eid} className="min-h-2000 my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3"><CVcard ed={ed}/></div>
+                    )
+                })
+            }
         }
 
 
@@ -38,10 +41,20 @@ class Cv extends Component {
             <div>
                 <h1 className="text-white font-sans ml-20 pt-6 pb-4">My Career summary...</h1>
                 {cvList}
+                {eduList}
             </div>
         )
     }
 
+    componentDidMount() {
+        axios.get('./src/data/cv.json')
+            .then(response => {
+                this.setState({
+                    cvs: response.data,
+                    edu: response.data
+                })
+            })
+}
 
 
 };
